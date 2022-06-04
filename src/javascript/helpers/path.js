f7869683b139a6f286f389c61784d1fcabdf5243
Elwin Bockstael <elwin@propane.nl>
2022-06-04 17:42:30 +0200

    Bugfix: query selector not valide because of `hacstag`

--- src/javascript/helpers/path.js
+++ src/javascript/helpers/path.js
@@ -10,7 +10,7 @@ export default class Path {
    */
   getPath = function () {
     const script = document
-      .querySelector('script[src$="fr24_card.js"]')
+      .querySelector('script[src*="fr24_card.js"]')
       .getAttribute("src");
     return script.substring(0, script.lastIndexOf("/") + 1);
   };
